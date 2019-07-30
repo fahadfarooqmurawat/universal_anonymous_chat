a4b8005d87e596c52a28caf69d5944069d38ce2e
fahadfarooqmurawat <fahadfarooqmurawat@gmail.com>
2019-07-30 18:20:41 +0500

    Changed test to send next test message after acknowlegement from server

--- public/js/io.js
+++ public/js/io.js
@@ -1,4 +1,4 @@
-var socketConnection=function(){var e=io({query:{user_name:settings.getName()}});return e.once("welcome",({count:e})=>{ui.render_welcomeMessage({count:e})}),e.on("usermessage",({sender_id:n,message:s,user_name:r,gen_time:t,server_time:m})=>{n===e.id?ui.render_myMessage({message:s,gen_time:t,server_time:m,end_time:Date.now()}):ui.render_othersMessage({message:s,user_name:r,gen_time:t,server_time:m,end_time:Date.now()})}),e.on("userconnected",e=>{ui.render_userConnected(e)}),e.on("userdisconnected",e=>{ui.render_userDisconnected(e)}),e.on("userrename",e=>{ui.render_userRename(e)}),{sendMessage:function(n,s){n&&e.emit("message",{message:n,gen_time:Date.now()},e=>{s&&s()})},updateName:function(){e.emit("rename",{new_name:settings.getName()})}}}();
+var socketConnection=function(){var e=io({query:{user_name:settings.getName()}});return e.once("welcome",({count:e})=>{ui.render_welcomeMessage({count:e})}),e.on("usermessage",({sender_id:n,message:s,user_name:r,gen_time:t,server_time:m})=>{n===e.id?ui.render_myMessage({message:s,gen_time:t,server_time:m,end_time:Date.now()}):ui.render_othersMessage({message:s,user_name:r,gen_time:t,server_time:m,end_time:Date.now()})}),e.on("userconnected",e=>{ui.render_userConnected(e)}),e.on("userdisconnected",e=>{ui.render_userDisconnected(e)}),e.on("userrename",e=>{ui.render_userRename(e)}),e.on("echo",n=>e.emit("echo",n)),{sendMessage:function(n,s){n&&e.emit("message",{message:n,gen_time:Date.now()},e=>{s&&s()})},updateName:function(){e.emit("rename",{new_name:settings.getName()})}}}();
 // var socketConnection = (function(){
 //   var socket = io({ query:{ user_name:settings.getName() } });
 //   function sendMessage(message,cb){
@@ -13,9 +13,9 @@ var socketConnection=function(){var e=io({query:{user_name:settings.getName()}})
 //   socket.on('userconnected',userObj=>{ui.render_userConnected(userObj);});
 //   socket.on('userdisconnected',userObj=>{ui.render_userDisconnected(userObj);});
 //   socket.on('userrename',userObj=>{ui.render_userRename(userObj);});
+//   socket.on('echo',msg=>socket.emit('echo',msg));
 //   return {
 //     sendMessage,
 //     updateName
 //   }
-// })(); 
-
+// })();
